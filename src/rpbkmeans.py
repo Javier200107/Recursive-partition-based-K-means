de95f313aa1ea0c86fd4a98d60aa56e76d1a5206
Javier200107 <xaviergonzalezbejar@gmail.com>
2024-03-22 01:05:24 +0100

    Start implementation

--- src/rpbkmeans.py
+++ src/rpbkmeans.py
@@ -1,17 +1,50 @@
 import numpy as np
 
+from subset import Subset
+
 class RecursivePartitionBasedKMeans:
     def __init__(self) -> None:
+        self.centroids = None
+        self.max_iterations = 100
+
+    def _create_partition(self, X, k):
         pass
 
-    def fit(self, X, k):
-        pass
+
+    def fit(self, X, k, threshold=0.01):
+        X = X.copy()
+
+        i = 0
+
+        while i < self.max_iterations:
+            
+            self._create_partition(X, k)
+
+            i += 1
+
+        return 
 
     def predict(self, X):
         pass
 
+    """def weighted_lloyd(self, X, w, k, centroids, threshold=0.01, max_iterations=100):
+        i = 0
+        while i < max_iterations:
+            # Assign each point to the nearest centroid
+            clusters = self.assign_points_to_clusters(X, centroids)
+
+            # Calculate the new centroids
+            new_centroids = self.calculate_new_centroids(X, w, clusters, k)
+
+            # Check for convergence
+            if np.max(self.centroids - np.array(new_centroids)):
+                break 
+
+            centroids = new_centroids
+            i += 1"""
+
 if __name__ == "__main__":
-    X = np.random.rand(100, 2)
+    X = np.random.rand(10, 2)
     rpbkmeans = RecursivePartitionBasedKMeans()
     rpbkmeans.fit(X, 3)
     print(rpbkmeans.predict(X))
---
+++ src/subset.py
@@ -0,0 +1,11 @@
+class Subset:
+    def __init__(self) -> None:
+        pass
+
+    def get_representative(self):
+        pass
+
+    
+
+
+    
